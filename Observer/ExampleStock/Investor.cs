5c92471de84266f3c07fb656d17e2e74108d4493
dcvieira <dcvieira@tjsp.jus.br>
2022-05-19 13:01:27 -0300

    add Observer Example

---
+++ Observer/ExampleStock/Investor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleStock
+{
+    public class Investor : IInvestor
+    {
+        private string _name;
+        public Stock Stock { get; set; }
+
+        public Investor(string name)
+        {
+            _name = name;
+        }
+
+
+        public void Update(Stock stock)
+        {
+            Console.WriteLine("Notified {0} of {1}'s " +
+                "change to {2:C}", _name, stock.Symbol, stock.Price);
+        }
+    }
+}
---
+++ Observer/ExampleStock/Stock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleStock
+{
+    public abstract class Stock
+    {
+        private string _symbol;
+        private double _price;
+        private List<IInvestor> investors = new List<IInvestor>();
+
+        public string Symbol 
+        {
+            get => _symbol; 
+        }
+
+        public double Price { 
+            get => _price; 
+            set
+            {
+                if(_price != value)
+                {
+                    _price = value;
+                    Notify();
+                }
+            } 
+        }
+
+        protected Stock(string symbol, double price)
+        {
+            _symbol = symbol;
+            _price = price;
+        }
+
+        public void Attach(IInvestor investor)
+        {
+            investors.Add(investor);
+        }
+
+        public void Detach(IInvestor investor)
+        {
+            investors.Remove(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (var investor in investors)
+            {
+                investor.Update(this);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
