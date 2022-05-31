241a8f66667bc8e33569a8015372fbfbdb68ab28
dcvieira <dcvieira@tjsp.jus.br>
2022-05-31 16:26:59 -0300

    Add Command pattern

---
+++ Command/ShoppingCart/ShoppingCart.Business/Commands/AddToCartCommand.cs
@@ -0,0 +1,50 @@
+﻿using ShoppingCart.Business.Models;
+using ShoppingCart.Business.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Business.Commands
+{
+    public class AddToCartCommand : ICommand
+    {
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly Product _product;
+
+        public AddToCartCommand(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository, Product product)
+        {
+            _shoppingCartRepository = shoppingCartRepository;
+            _productRepository = productRepository;
+            _product = product;
+        }
+        public bool CanExecute()
+        {
+           if (_product == null) return false;
+
+            return _productRepository.GetStockFor(_product.ArticleId) > 0;
+        }
+
+        public void Execute()
+        {
+            if (_product == null) return;
+
+            _productRepository.DecreaseStockBy(_product.ArticleId, 1);
+
+            _shoppingCartRepository.Add(_product);
+        }
+
+        public void Undo()
+        {
+            if (_product == null) return;
+
+            var lineItem = _shoppingCartRepository.Get(_product.ArticleId);
+
+            _productRepository.IncreaseStockBy(_product.ArticleId, lineItem.Quantity);
+
+            _shoppingCartRepository.RemoveAll(_product.ArticleId);
+        }
+    }
+}
